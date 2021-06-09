560bdbf011ad1af391e7248e9075ffbbd1214a27
Midka <me@midka.dev>
2021-06-09 17:04:02 +0300

    Hopefully fixxed babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,26 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo', '@babel/preset-typescript'],
     plugins: [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          '@assets': './app/assets',
-          '@components': './app/components',
-          '@config': './app/config',
-          '@utils': './app/utils/',
-          '@screens': './app/screens',
-          '@hooks': './app/hooks',
-          '@navigation': './app/navigation',
-          '@api': './app/api',
-          '@auth': './app/auth',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@assets': './app/assets',
+            '@components': './app/components',
+            '@config': './app/config',
+            '@utils': './app/utils/',
+            '@screens': './app/screens',
+            '@hooks': './app/hooks',
+            '@navigation': './app/navigation',
+            '@api': './app/api',
+            '@auth': './app/auth',
+          },
         },
-      },
+      ],
     ],
   };
 };
